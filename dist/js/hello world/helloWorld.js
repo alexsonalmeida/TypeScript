e906e13a4ed59007899c524098182d1470b2534d
alexsonalmeida <101877352+alexsonalmeida@users.noreply.github.com>
2022-08-12 09:21:50 -0300

    testes iniciais da linguagem

---
+++ dist/js/hello world/helloWorld.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const msg = "hello world";
+teste(msg);
+function teste(msg) {
+    console.log(msg);
+}
+exports.default = teste;
---
+++ src/hello world/helloWorld.ts
@@ -0,0 +1,8 @@
+const msg: string = "hello world"
+teste(msg)
+
+function teste(msg: string): void {
+    console.log(msg)
+}
+
+export default teste
